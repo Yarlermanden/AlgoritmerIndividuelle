3ec61b329b7b6bbef391dc00dc44a7e8e113baa0
Martin Meldgaard Holst <martinmeldgaardholst@Martins-MacBook-Pro.local>
2021-10-19 20:46:08 +0200

    Refactor Paintball into methods. Haven't made it depthfirst searching yet

--- NetworkFlow/Paintball.cs
+++ NetworkFlow/Paintball.cs
@@ -1,59 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 public class Paintball
 {
-    public static void Main(string[] args) {
+    private static int _n;
+    private static int _m;
+    private static Node _source;
+    private static Node _sink;
+    private static Node[] _playersToShoot;
+    private static Node[] _playersToBeShot;
+    private static int[] _results;
+
+    public static void Main(string[] args)
+    {
         var lineArr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-        var n = lineArr[0];
-        var m = lineArr[1];
-
-        var playersToShoot = new Node[n];
-        var playersToBeShot = new Node[n];
-        for (int i = 0; i < n; i++) playersToShoot[i] = new Node(i);
-        for (int i = 0; i < n; i++) playersToBeShot[i] = new Node(i);
-
-        for (int i = 0; i < m; i++)
-        {
-            lineArr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-            var p1ToShoot = playersToShoot[lineArr[0]-1];
-            var p1ToBeShot = playersToBeShot[lineArr[0]-1];
-            var p2ToShoot = playersToShoot[lineArr[1]-1];
-            var p2ToBeShot = playersToBeShot[lineArr[1]-1];
-            
-            p1ToShoot.OutgoingEdges.Add(new Edge(p1ToShoot, p2ToBeShot, 1));
-            p2ToShoot.OutgoingEdges.Add(new Edge(p2ToShoot, p1ToBeShot, 1));
-            
-            p2ToBeShot.OutgoingEdges.Add(new Edge(p2ToBeShot, p1ToShoot, 0, true));
-            p1ToBeShot.OutgoingEdges.Add(new Edge(p1ToBeShot, p2ToShoot, 0, true));
-        }
-
-        if (n > m)
+        _n = lineArr[0];
+        _m = lineArr[1];
+        ReadInputNodesAndBuildNetwork();
+        if (_n > _m)
         {
             Console.WriteLine("Impossible");
             return;
         }
 
-        //Add from source to all playersToShoot
-        //Add sink to all playersToBeShot
-        var source = new Node(-1);
-        var sink = new Node(-2);
-        for (int i = 0; i < n; i++)
+        AddSourceAndSinkToNetwork();
+        AugmentNetwork();
+        OrderResultForOutput();
+        PrintOutput();
+    }
+
+    private static void ReadInputNodesAndBuildNetwork()
+    {
+        _playersToShoot = new Node[_n];
+        _playersToBeShot = new Node[_n];
+        for (int i = 0; i < _n; i++) _playersToShoot[i] = new Node(i);
+        for (int i = 0; i < _n; i++) _playersToBeShot[i] = new Node(i);
+
+        for (int i = 0; i < _m; i++)
         {
-            source.OutgoingEdges.Add(new Edge(source, playersToShoot[i], 1));
-            playersToShoot[i].OutgoingEdges.Add(new Edge(playersToShoot[i], source, 1, true));
-            
-            playersToBeShot[i].OutgoingEdges.Add(new Edge(playersToBeShot[i], sink, 1));
-            sink.OutgoingEdges.Add(new Edge(sink, playersToBeShot[i], 1, true));
+            var lineArr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            var p1ToShoot = _playersToShoot[lineArr[0] - 1];
+            var p1ToBeShot = _playersToBeShot[lineArr[0] - 1];
+            var p2ToShoot = _playersToShoot[lineArr[1] - 1];
+            var p2ToBeShot = _playersToBeShot[lineArr[1] - 1];
+
+            p1ToShoot.OutgoingEdges.Add(new Edge(p1ToShoot, p2ToBeShot, 1));
+            p2ToShoot.OutgoingEdges.Add(new Edge(p2ToShoot, p1ToBeShot, 1));
+
+            p2ToBeShot.OutgoingEdges.Add(new Edge(p2ToBeShot, p1ToShoot, 0, true));
+            p1ToBeShot.OutgoingEdges.Add(new Edge(p1ToBeShot, p2ToShoot, 0, true));
         }
+    }
 
+    private static void AddSourceAndSinkToNetwork()
+    {
+        _source = new Node(-1);
+        _sink = new Node(-2);
+        for (int i = 0; i < _n; i++)
+        {
+            _source.OutgoingEdges.Add(new Edge(_source, _playersToShoot[i], 1));
+            _playersToShoot[i].OutgoingEdges.Add(new Edge(_playersToShoot[i], _source, 1, true));
 
-        while (true)    //augment by finding best path
+            _playersToBeShot[i].OutgoingEdges.Add(new Edge(_playersToBeShot[i], _sink, 1));
+            _sink.OutgoingEdges.Add(new Edge(_sink, _playersToBeShot[i], 1, true));
+        }
+    }
+
+    private static void AugmentNetwork()
+    {
+        while (true) //augment by finding best path
         {
             var queue = new Queue<(Node, Node)>();
             var visited = new HashSet<Node>();
             var previous = new Dictionary<Node, Node>();
-            queue.Enqueue((source, null));
+            queue.Enqueue((_source, null));
             Node currentNode;
             Node previousNode;
             do //find best path - breadth first search
@@ -61,23 +82,23 @@ public class Paintball
                 (currentNode, previousNode) = queue.Dequeue();
                 if (visited.Contains(currentNode)) continue;
                 previous.Add(currentNode, previousNode);
-                if (currentNode == sink) break;
+                if (currentNode == _sink) break;
                 visited.Add(currentNode);
 
                 foreach (var e in currentNode.OutgoingEdges)
                 {
                     if (e.Capacity > 0)
                     {
-                        queue.Enqueue((e.To, currentNode)); 
+                        queue.Enqueue((e.To, currentNode));
                     }
                 }
             } while (queue.Count > 0);
 
-            if (currentNode != sink) break;
-            
+            if (currentNode != _sink) break;
+
             //push flow through this path
-            currentNode = sink;
-            while (currentNode != source)
+            currentNode = _sink;
+            while (currentNode != _source)
             {
                 previousNode = previous[currentNode];
                 var edge1 = previousNode.OutgoingEdges.Single(edge => edge.To == currentNode);
@@ -90,27 +111,42 @@ public class Paintball
                 currentNode = previousNode;
             }
         }
+    }
 
-        //Run from source to sink.
-        //All the edges in second round which capacity is 0 represents which nodes shot eachother
-        var results = new int[n];
-        foreach (var e1 in source.OutgoingEdges)
+    private static void FindPossiblePath()
+    {
+
+    }
+
+    private static void PushFlow()
+    {
+
+    }
+
+    private static void OrderResultForOutput()
+    {
+        _results = new int[_n];
+        foreach (var e1 in _source.OutgoingEdges)
         {
             foreach (var e2 in e1.To.OutgoingEdges)
             {
                 if (e2.Capacity == 0 && !e2.Residual)
                 {
-                    results[e2.From.Id] = e2.To.Id + 1;
+                    _results[e2.From.Id] = e2.To.Id + 1;
                 }
             }
         }
+    }
 
-        if (results.Any(x => x == 0))
+    private static void PrintOutput()
+    {
+        if (_results.Any(x => x == 0))
         {
             Console.WriteLine("Impossible");
             return;
         }
-        for(int i = 0; i < results.Length; i++) Console.WriteLine(results[i]);
+
+        for (int i = 0; i < _results.Length; i++) Console.WriteLine(_results[i]);
     }
 }
 
