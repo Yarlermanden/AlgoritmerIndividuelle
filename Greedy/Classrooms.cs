9ccb56dbc75e7d512a1b53ce80c0aef89e6b44ea
yarlermanden <mmho@itu.dk>
2021-12-09 16:42:07 +0100

    Tried Classrooms with SortedSet (red/black binary tree) though it isn't fast enough without a direct search function, which isn't possible due to shitty Microsoft developers

--- Greedy/Classrooms.cs
+++ Greedy/Classrooms.cs
@@ -19,46 +19,26 @@ public class Classrooms
             a = Console.ReadLine().Split(' ');
             s = int.Parse(a[0]);
             f = int.Parse(a[1]);
-            activities[i] = new Activity(s, f);
+            activities[i] = new Activity(s, f, i);
         }
         if(n == k) {Console.WriteLine(n); return;}
         Array.Sort(activities, (x, y) => x.Finish.CompareTo(y.Finish));
 
-        LinkedList rooms = new LinkedList();
+        SortedSet<Activity> rooms = new SortedSet<Activity>(new Comparer());
         int count = 0;
         foreach(Activity current in activities)
         {
-            if (rooms.Last != null && rooms.Last.Value.Finish < current.Start)
+            //if (rooms.Last != null && rooms.Last.Value.Finish < current.Start)
+            if(rooms.Count > 0 && rooms.Min.Finish < current.Start)
             {
-                LinkedListNode previousNode = null;
-                var node = rooms.First;
-                while (node != null)
-                {
-                    var next = node.Next;
-                    if (node.Value.Finish < current.Start)
-                    {
-                        count++;
-                        if (previousNode == null)
-                        {
-                            if (rooms.First == rooms.Last) rooms.Last = next;
-                            rooms.First = next;
-                        }
-                        else
-                        {
-                            if(node == rooms.Last) rooms.Last = previousNode;
-                            previousNode.Next = next;
-                        }
-                        rooms.AddFirst(current);
-                        break;
-                    }
-                    previousNode = node;
-                    node = next;
-                }
+                var set = rooms.GetViewBetween(new Activity(0, 0, 0), new Activity(0, current.Start, 0));
+                rooms.Remove(set.Max);
+                rooms.Add(current);
+                count++;
             }
             else if (rooms.Count < k)
             {
-                rooms.AddFirst(current);
-                rooms.Count++;
+                rooms.Add(current);
                 count++;
             }
         }
@@ -66,44 +46,22 @@ public class Classrooms
     }
     private class Activity
     {
+        public int Count { get; set; }
         public int Start { get; }
         public int Finish { get; }
-        public Activity(int s, int f)
+        public Activity(int s, int f, int count)
         {
+            Count = count;
             Start = s;
             Finish = f;
         }
     }
 
-    private class LinkedList
+    private class Comparer : IComparer<Activity>
     {
-        public LinkedListNode First { get; set; }
-        public LinkedListNode Last { get; set; }
-        public int Count { get; set; }
-
-        public void AddFirst(Activity a)
+        public int Compare(Activity a, Activity b)
         {
-            var node = new LinkedListNode(a);
-            if (First != null)
-            {
-                node.Next = First;
-                First = node;
-            }
-            else
-            {
-                First = node;
-                Last = node;
-            }
-        }
-    }
-
-    private class LinkedListNode
-    {
-        public Activity Value { get; set; }
-        public LinkedListNode Next { get; set; }
-        public LinkedListNode(Activity a)
-        {
-            Value = a;
+            return (a.Finish != b.Finish) ? a.Finish.CompareTo(b.Finish) : a.Count.CompareTo(b.Count);
         }
     }
 }
